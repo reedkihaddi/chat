ab54436c75c218cfe5db356c650527fa91a03356
Saurabh <saurabhbraryo@gmail.com>
2020-09-11 23:20:15 +0530

    first

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
